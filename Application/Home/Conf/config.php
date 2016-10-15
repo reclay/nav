c36184c743a7c636c7975bff6aa2c46daaa09d24
clay <2868902382@qq.com>
2016-10-15 13:39:29 +0800

    Update config.php

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -1,12 +1,5 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
-	'DB_TYPE' => 'mysql', // 数据库类型
-	'DB_HOST' => 'qdm170255637.my3w.com', // 服务器地址
-	'DB_NAME' => 'qdm170255637_db', // 数据库名
-	'DB_USER' => 'qdm170255637', // 用户名
-	'DB_PWD' => 'lj160033', // 密码
-	'DB_PORT' => 3306, // 端口
-	'DB_PREFIX' => 'nav_', // 数据库表前缀
-	'DB_CHARSET' => 'utf8', // 字符集
+	
 );
